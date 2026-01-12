b5a9b067268d0b8c3bd44acadad6d0db2657a1a7
Sascha <ssommer0510@gmail.com>
2026-01-12 20:48:56 +0100

    const mutation added

--- .idea/test/mutation.js
+++ .idea/test/mutation.js
@@ -1,5 +1,14 @@
-const mutation = {
-    id: 123,
-    star rating: 1,
-    heuristic score: 0.89,
+
+function buildMutations() {
+    const newDiv = document.createElement("div");
+    const mutation = {
+        id: 123,
+        star rating: 1,
+        heuristic score: 0.89,
+    }
+    for(let i = 0; i < 25; i++) {
+
+
+
+    }
 }
