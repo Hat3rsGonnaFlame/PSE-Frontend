cff1b1c15013d3bfc03b05e6e753d24d9279a39b
Sascha <ssommer0510@gmail.com>
2026-01-13 13:26:13 +0100

    const mutation added

--- .idea/test/mutation.js
+++ .idea/test/mutation.js
@@ -1,13 +1,14 @@
 
 function buildMutations() {
-    const newDiv = document.createElement("div");
+    const mutationdiv = document.querySelector(".mutation")
     const mutation = {
         id: 123,
         star rating: 1,
         heuristic score: 0.89,
     }
     for(let i = 0; i < 25; i++) {
-
+        mutationdiv.innerHTML +=
+        "<h2>Mutation#" + mutation.id + "</h2>"
 
 
     }
