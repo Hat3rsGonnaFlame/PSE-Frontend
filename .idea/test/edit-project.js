753cae04ed3f605da0e1f7783029953aa9fd32e7
jerome <uwuul@student.kit.edu>
2026-01-13 17:06:48 +0100

    changes for the popup

--- .idea/test/edit-project.js
+++ .idea/test/edit-project.js
@@ -1,10 +1,12 @@
-const edit = document.getElementById("edit");
+const edit = document.querySelectorAll(".edit");
 const modal_container = document.getElementById("modal_container");
 const save = document.getElementById("save");
 
-edit.addEventListener("click", () => {
-    modal_container.classList.add("show");
-})
+edit.forEach(button => {
+    button.addEventListener("click", () => {
+        modal_container.classList.add("show");
+    });
+});
 
 save.addEventListener("click", () => {
     modal_container.classList.remove("show");
