157e2055f6e2badb663b7488386ab16eaa58d399
jerome <uwuul@student.kit.edu>
2026-01-13 16:34:21 +0100

    popup added without anything yet

--- .idea/test/edit-project.js
+++ .idea/test/edit-project.js
@@ -1,1 +1,11 @@
-const editButton = document.getElementById("edit");
+const edit = document.getElementById("edit");
+const modal_container = document.getElementById("modal_container");
+const save = document.getElementById("save");
+
+edit.addEventListener("click", () => {
+    modal_container.classList.add("show");
+})
+
+save.addEventListener("click", () => {
+    modal_container.classList.remove("show");
+})
